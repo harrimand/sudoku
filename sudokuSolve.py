5ce59471ccd6d4f15c3ae0601213bfa2cc5b09a5
harrimand <harrimand@gmail.com>
2020-02-21 12:12:33 -0800

    first commit

---
+++ sudokuSolve.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+def possible(y, x, n):
+    global grid
+    if n in [grid[y][c] for c in range(9)]:
+        return False
+    if n in [grid[r][x] for r in range(9)]:
+        return False
+    return n not in [[grid[r + y//3*3][c + x//3*3] for c in range(3)] for r in range(3)]
+
+def possible2(y, x, n):
+    global grid
+    for i in range(0, 9):
+        if grid[y][i] == n:
+            return False
+    for i in range(0, 9):
+        if grid[i][x] == n:
+            return False
+
+    x0 = (x//3)*3
+    y0 = (y//3)*3
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if grid[y0 + i][x0 + j] == n:
+                return False
+    return True
+
+def solve():
+    global grid
+    for y in range(9):
+        for x in range(9):
+            if grid[y][x] == 0:
+                for n in range(1, 10):
+                    if possible(y, x, n):
+                        grid[y][x] = n
+                        solve()
+                        grid[y][x] = 0
+                return
+    print(np.matrix(grid))
+    input("more?")
+
+'''
+grid = [[5,3,0,0,7,0,0,0,0],
+        [6,0,0,1,9,5,0,0,0],
+        [0,9,8,0,0,0,0,6,0],
+        [8,0,0,0,6,0,0,0,3],
+        [4,0,0,8,0,3,0,0,1],
+        [7,0,0,0,2,0,0,0,6],
+        [0,6,0,0,0,0,2,8,0],
+        [0,0,0,4,1,9,0,0,5],
+        [0,0,0,0,8,0,0,0,0]]
+'''
+
+grid = np.zeros((9,9), dtype=int)
+
+solve()
+
+
